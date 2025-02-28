da1f22b19e717f9a2e128c46d4720e3072fcea50
Amrutha294 <amrutha.29.2020@gmail.com>
2025-02-28 10:16:15 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ st.write("Welcome to the my Chatbot! How Can i help you?")
 if "message" not in st.session_state:
   st.session_state.message=[]
 
-for message in st.session_state.messages:
+for message in st.session_state.message:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
